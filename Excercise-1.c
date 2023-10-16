987d4728053d14b05212c5bb5a5d6b908b3c97e1
mike2023choi <john91528.choi@gmail.com>
2023-10-16 11:17:13 +0900

    Add code to check C compiler version

--- Excercise-1.c
+++ Excercise-1.c
@@ -5,5 +5,6 @@
      printf("Name   : Alexandra Abramov\n"); 
      printf("DOB    : July 14, 1975\n"); 
      printf("Mobile : 99-9999999999\n"); 
-     return(0); 
+   
+     printf ("%ld", __STDC_VERSION__);
   }
