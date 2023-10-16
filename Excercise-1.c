edec3611a1064f0c8305a24f4270f2b7cad6fb75
mike2023choi <john91528.choi@gmail.com>
2023-10-16 13:18:19 +0900

    add new code

--- Excercise-1.c
+++ Excercise-1.c
@@ -5,6 +5,8 @@
      printf("Name   : Alexandra Abramov\n"); 
      printf("DOB    : July 14, 1975\n"); 
      printf("Mobile : 99-9999999999\n"); 
+
+     printf("%ld", __STDC_VERSION__);
    
      return(0);
   }
